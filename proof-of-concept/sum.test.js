7ddc47eb07684d0517c2d80247a22870edb7b17f
Crom <daffodil.luck9@gmail.com>
2024-08-05 12:15:29 +0100

    ticket 3 edited complete

---
+++ proof-of-concept/sum.test.js
@@ -0,0 +1,16 @@
+//  import `test` and `expect` functions from Vitest
+import { expect, test } from "vitest";
+
+
+//  import the `sum` function from sum.js
+import { sum } from "/sum.js";
+
+//  create a bare bones test
+test('bare bones test', () => {
+    expect(sum(5,7).toBe(12))
+})
+
+//  run Vitest to check that it detects and passes the new test
+//  inside the test:
+//      call the sum function with arguments: 5 and 7
+//      assert that the value returned is 12
