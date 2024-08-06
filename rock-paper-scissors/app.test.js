95f845ff2bbb654301ae9d4eb4c6ed83a0a19bdb
Elspeth <elspeth@mardaljevic.org>
2024-08-06 10:46:22 +0100

    ticket 5 test fail

---
+++ rock-paper-scissors/app.test.js
@@ -0,0 +1,11 @@
+import { expect, test } from “vitest”;
+
+
+
+import { ROCK, PAPER, SCISSORS, DRAW, WIN, LOSS } from "/app.js";
+
+import { calculateRoundResult,generateComputerMove, calculateNewScores } from "/app.js";
+
+test('draw one' , () => {
+    expect (calculateRoundResult(ROCK, ROCK)).toBe({outcome: DRAW, message: "Both players chose rock. It's a draw." })
+})
